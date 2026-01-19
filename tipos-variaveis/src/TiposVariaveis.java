603107b3f78525a2d904873c7db2dec4360c0b2f
daniellinslopes <daniboy237@proton.me>
2026-01-19 18:05:46 -0300

    atualizando projeto

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,10 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
 
         int numero = 5;
 
@@ -13,5 +15,6 @@ public class TiposVariaveis {
         System.out.println(numero);
 
         final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
